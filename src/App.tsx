0ba1aeaf31226b4b04be2d4d10a8450d45a54986
ToanNgo2709 <ngotoanlibra@gmail.com>
2023-05-20 09:36:24 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome To the fullstack course
-     </h1>
-    </div>
+    <Navbar/>
   );
 }
 
